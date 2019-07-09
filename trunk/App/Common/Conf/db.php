98dfdf785642a26e0f3363864c15a813e5fdf8c3
DESKTOP-288T8RJ\11270 <1127022238@qq.com>
2019-07-09 21:57:21 +0800

    忽略数据库配置

--- trunk/App/Common/Conf/db.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'DB_TYPE' => 'mysqli',
-  'DB_HOST' => '127.0.0.1',
-  'DB_USER' => 'root',
-  'DB_PWD' => 'root',
-  'DB_NAME' => '46_xzl',
-  'DB_PREFIX' => 'bestop_',
-);?>
