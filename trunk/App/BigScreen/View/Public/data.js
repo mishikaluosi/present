bf79e0cdc7310cd5b8a904d2fbb9a14b079caafe
yangzhibiao <yangzb@thinkerx.com>
2019-07-15 10:22:51 +0800

    添加大屏显示模块

---
+++ trunk/App/BigScreen/View/Public/data.js
@@ -0,0 +1,11 @@
+var personArray = new Array;
+personArray.push({id: 546, image: "img/tx.png", thumb_image: "img/tx.png", name: "赞木赞木赞木赞木赞木赞木赞木赞木赞木赞木赞木赞木111"});
+personArray.push({id: 722, image: "img/tx1.png", thumb_image: "img/tx1.png", name: "小超"});
+personArray.push({id: 1044, image: "img/tx2.png", thumb_image: "img/tx2.png", name: "saray鱼"});
+personArray.push({id: 1051, image: "img/tx3.png", thumb_image: "img/tx3.png", name: "Lily"});
+personArray.push({id: 1711, image: "img/tx4.png", thumb_image: "img/tx4.png", name: "钟道江"});
+personArray.push({id: 1717, image: "img/tx5.png", thumb_image: "img/tx5.png", name: "初见影视-亮亮"});
+personArray.push({id: 1718, image: "img/tx6.png", thumb_image: "img/tx6.png", name: "初见影视团队 --小涛"});
+personArray.push({id: 1730, image: "img/tx.png", thumb_image: "img/tx.png", name: "飞翔"});
+personArray.push({id: 1732, image: "img/tx1.png", thumb_image: "img/tx1.png", name: "左慧子"});
+personArray.push({id: 1738, image: "img/tx2.png", thumb_image: "img/tx2.png", name: "园园妹纸"});
