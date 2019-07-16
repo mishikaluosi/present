059ff0ee4a6f94739a399e76d6613ea542842bbd
yangzhibiao <1127022238@qq.com>
2019-07-16 10:45:10 +0800

    样式修改

--- trunk/App/BigScreen/Controller/IndexController.class.php
+++ trunk/App/BigScreen/Controller/IndexController.class.php
@@ -5,7 +5,14 @@ class IndexController extends Controller
 {
     public function index()
     {
+        $e_id = I('e_id');
+        if(!$e_id){
+            echo '页面错误';
+            exit();
+        }
+//        $sql = ""
         $this->display();
     }
+
 
 }
